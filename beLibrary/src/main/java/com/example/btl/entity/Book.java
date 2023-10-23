24da4896a8e80200aed5c1158c29de7661f013ab
Nguyen Tan <riptan2001@gmail.com>
2023-10-23 23:10:24 +0700

    create book entity

---
+++ beLibrary/src/main/java/com/example/btl/entity/Book.java
@@ -0,0 +1,15 @@
+package com.example.btl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+    @Id
+    private int id;
+}
